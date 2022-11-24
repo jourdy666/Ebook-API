a17490bf08d6ee89f69fe43900c414d14383085d
agus08 <3103120119@student.skmktelkom-pwt.sch.id>
2022-11-24 08:37:35 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/Books/{id}', [BookController::class, 'show']);
+Route::get('/Authors', [AuthorController::class, 'index']);
+Route::get('/Authors/{id}', [AuthorController::class, 'show']);
+
+// protected routes
+Route::middleware('auth:sanctum')->group(function (){
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthController::class)->except('create', 'edit', 'show', 'index');
+});
+
 Route::get('me', [AuthController::class, 'me']);
 
 Route::resource('books', BookController::class)->except(
